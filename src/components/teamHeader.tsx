4eb04a6a5173643f162f1dbbec4539e0d0787c97
iYuno <roma_loginov1@list.ru>
2024-03-27 02:48:46 +0400

    add dropdown user menu

--- src/components/teamHeader.tsx
+++ src/components/teamHeader.tsx
@@ -1,3 +1,4 @@
+'use server'
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,14 +7,26 @@ import {
   BreadcrumbSeparator
 } from '@/components/ui/breadcrumb';
 import Link from 'next/link';
-import { Plus, Slash, UserPlus } from 'lucide-react';
+import { Folders, Github, Plus, Settings, Slash, User, UserPlus, Users } from 'lucide-react';
 import ThemeButton from '@/components/ui/themeButton';
 import { getTeamSpaceInfo } from '@/lib/data';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { currentUser } from '@/lib/auth';
 
 export default async function TeamHeader({ teamSpaceId, projectId }: {teamSpaceId: string, projectId: string}) {
 
-  const { projectName, teamSpace } = await getTeamSpaceInfo(teamSpaceId)
+  const getTeamPromise = getTeamSpaceInfo(teamSpaceId)
+  const userPromise = currentUser()
+
+  const [{projectName, teamSpace}, user] = await Promise.all([getTeamPromise, userPromise])
 
   return (
     <>
@@ -39,10 +52,48 @@ export default async function TeamHeader({ teamSpaceId, projectId }: {teamSpaceI
         </Breadcrumb>
         <div className="flex items-center gap-x-2">
           <ThemeButton/>
-          <Avatar className="size-7">
-            <AvatarImage src=""/>
-            <AvatarFallback>RL</AvatarFallback>
-          </Avatar>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar className="size-7">
+                <AvatarImage src=""/>
+                <AvatarFallback>RL</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-48 rounded-sm">
+              <DropdownMenuLabel>
+                {user?.name}
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator/>
+              <DropdownMenuItem>
+                <User className="size-4 mr-2"/>
+                <span>Profile</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Settings className="size-4 mr-2"/>
+                <span>Settings</span>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator/>
+              <DropdownMenuItem>
+                <Folders className="size-4 mr-2"/>
+                <span>Projects</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Users className="size-4 mr-2"/>
+                <span>Teams</span>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator/>
+              <DropdownMenuItem>
+                <Github className="size-4 mr-2"/>
+                <span>
+                  Github
+                </span>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator/>
+              <DropdownMenuItem className="text-red-500 hover:!text-red-500">
+                Log out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
       <div className="flex w-full my-2 justify-between items-center">
@@ -50,10 +101,11 @@ export default async function TeamHeader({ teamSpaceId, projectId }: {teamSpaceI
           {teamSpace?.teamName}
         </h2>
         <div className="gap-2 flex font-montserrat text-sm h-fit items-center">
-          <button type="button" className="flex gap-2 items-center">
-            <p className="uppercase text-neutral-400">
+          <div className="flex gap-2 items-center">
+            <button
+              className="uppercase text-neutral-400 hover:text-neutral-50 hover:underline transition-all ease-out">
               {teamSpace.totalUsersAmount} {teamSpace.totalUsersAmount > 1 ? 'users' : 'user'}:
-            </p>
+            </button>
             <div className="flex -space-x-4">
               {
                 teamSpace.userPreview.map((user, index, array) => (
@@ -67,7 +119,7 @@ export default async function TeamHeader({ teamSpaceId, projectId }: {teamSpaceI
                 ))
               }
             </div>
-          </button>
+          </div>
           <span className="w-[1px] h-4 bg-neutral-300 dark:bg-neutral-700"/>
           <button className="interactiveButton flex items-center space-x-1">
             <UserPlus className="size-4 stroke-2 stroke-current"/>
